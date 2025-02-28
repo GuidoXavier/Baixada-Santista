836828f78ed5a295252ec400fc5c45152a3bbe46
Ramon Garcia <ramonsantos.g@hotmail.com>
2025-02-28 00:38:44 -0300

    metodos para buscar usuario por id e metodo de editar usuario

--- src/main/java/TADS_SEM4_BACK_END/Baixada_Santista/Controladores/UserController.java
+++ src/main/java/TADS_SEM4_BACK_END/Baixada_Santista/Controladores/UserController.java
@@ -29,5 +29,29 @@ public class UserController {
         return ResponseEntity.ok(repository.findAll());
     }
 
+    //Recebo o usuário pelo ID, procuro o id, se eu achar eu devolvo os dados dele
+    @GetMapping("/{id}")
+    public ResponseEntity<User> buscarUsuarioPorId(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    //Recebo o usuário que vou editar pelo ID e recebo os novos dados pelo RequestBody
+    //Procuro o ID e se encontrar eu atualizo com os novos dados
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<User> editarUsuario(@PathVariable Long id, @RequestBody User usuarioAtualizado) {
+        return repository.findById(id)
+                .map(usuarioExistente -> {
+                    usuarioExistente.setNickname(usuarioAtualizado.getNickname());
+                    usuarioExistente.setEmail(usuarioAtualizado.getEmail());
+                    usuarioExistente.setRole(usuarioAtualizado.getRole());
+                    usuarioExistente.setAtivo(usuarioAtualizado.getAtivo());
+                    usuarioExistente.setSenha(usuarioAtualizado.getSenha());
+                    User usuarioSalvo = repository.save(usuarioExistente);
+                    return ResponseEntity.ok(usuarioSalvo);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
 
