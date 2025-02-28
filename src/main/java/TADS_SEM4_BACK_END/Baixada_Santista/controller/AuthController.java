2b9d00c2561a33545d41af97365da7ec351b64d0
Aureo Dantas <dantasaureo19@gmail.com>
2025-02-28 10:57:55 -0300

    login

--- src/main/java/TADS_SEM4_BACK_END/Baixada_Santista/controller/AuthController.java
+++ src/main/java/TADS_SEM4_BACK_END/Baixada_Santista/controller/AuthController.java
@@ -1,30 +1,43 @@
 package TADS_SEM4_BACK_END.Baixada_Santista.controller;
 
 import TADS_SEM4_BACK_END.Baixada_Santista.dto.AuthRequestDTO;
-import TADS_SEM4_BACK_END.Baixada_Santista.model.User;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import TADS_SEM4_BACK_END.Baixada_Santista.repository.UserRepository;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
 @CrossOrigin("*")
+@Slf4j
 public class AuthController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
-
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping
-    @JsonFormat
-    public ResponseEntity authenticate(@RequestBody @Valid AuthRequestDTO request) {
-        var token = new UsernamePasswordAuthenticationToken(request.email(), request.senha());
-        var authenticated = authenticationManager.authenticate(token);
-        return ResponseEntity.ok((User) authenticated.getPrincipal());
+    public ResponseEntity<String> authenticate(@RequestBody @Valid AuthRequestDTO request) {
+        try {
+            var user = userRepository.findByEmail(request.email()).get();
+            log.info(String.valueOf(passwordEncoder.encode(request.senha()).equals(user.getSenha())));
+
+            if (passwordEncoder.matches(request.senha(),user.getSenha()) && user.getEmail().equals(request.email())) {
+
+                return ResponseEntity.ok("Usuário autenticado");
+            } else {
+                return ResponseEntity.status(401).body("Email ou senha inválido");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body("Emaio");
+        }
     }
 }
 
--- src/main/java/TADS_SEM4_BACK_END/Baixada_Santista/model/Roles.java
+++ src/main/java/TADS_SEM4_BACK_END/Baixada_Santista/model/Roles.java
@@ -1,5 +1,5 @@
 package TADS_SEM4_BACK_END.Baixada_Santista.model;
 
 public enum Roles {
-    USER,ADMIN,STOCKIST;
+    USER,ADMIN,STOCKIST
 }
